be000b270a30a558b910a0e19ec1cbd7aa8b2db4
Sean Colandrea <link50@gmail.com>
2013-10-03 12:45:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChicagoShooting::Application.config.secret_key_base = '9c9af8e2b873861dcc5691bf1b0d41e3bbdfa9ea187230680845ac3b610325d0488f20f254c2b0e6217c974ce034083510692ebb14ad9c29af353fac4e913143'
